e92b03ad85b07ce3e598f2fdbbd51c9c22a4059a
阮连军 <ruanlianjun@pdx.local>
2023-04-19 18:33:25 +0800

    fix bug

--- example/handler/demo.go
+++ example/handler/demo.go
@@ -13,7 +13,19 @@ import (
 // @Success 200 {object} string "成功"
 // @Failure 400 {object} string "请求错误"
 // @Failure 500 {object} string "内部错误"
-// @Router /demo [get]
+// @Router /home [DELETE]
 func Hello(ctx echo.Context) error {
+	return ctx.String(http.StatusOK, "hello")
+}
+
+// Hello2
+// @Summary 获取单个文章
+// @Produce json
+// @Param id path int true "文章ID"
+// @Success 200 {object} string "成功"
+// @Failure 400 {object} string "请求错误"
+// @Failure 500 {object} string "内部错误"
+// @Router /home2 [POST]
+func Hello2(ctx echo.Context) error {
 	return ctx.String(http.StatusOK, "hello")
 }
--- example/main.go
+++ example/main.go
@@ -24,30 +24,28 @@ import (
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host petstore.swagger.io
+// @host 127.0.0.1:8082
 // @BasePath /v2
 func main() {
 	e := echo.New()
 	e.Pre(echo_swagger.Swagger(
 		echo_swagger.WithSwaggerFilename("swagger/swagger.json"),
+		echo_swagger.Refresh(true),
 	))
 
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 
-	e.GET("/demo", handler.Hello)
+	e.GET("/home", handler.Hello)
+	e.GET("/home2", handler.Hello2)
 
 	e.HTTPErrorHandler = func(err error, context echo.Context) {
 		_, _ = fmt.Fprintf(os.Stdout, "URl:%s Method:%s Message:%s\n", context.Request().URL, context.Request().Method, err.Error())
 	}
 
-	svc, err := echo_swagger.Start(e, ":1323", true)
+	err := echo_swagger.Start(e, ":1323", true)
 	if err != nil {
 		log.Fatal(err)
 	}
-	if err := svc.ListenAndServe(); err != nil {
-		log.Fatal(err)
-	}
-	e.Logger.Fatal()
 }
